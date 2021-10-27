78f24912432a59e6cd6861019366156c3b73777a
Bernardo Bevilaqua <bernardo.bfg@hotmail.com>
2021-10-27 00:06:18 -0300

    add max-width to contact us page

--- src/pages/ContactUs/styles.js
+++ src/pages/ContactUs/styles.js
@@ -10,6 +10,7 @@ export const Container = styled.div`
 
 export const Dashboard = styled.div`
   width: 90vw;
+  max-width: 1500px;
   margin: 15px auto 60px;
   background-color: #fff;
   display: flex;
